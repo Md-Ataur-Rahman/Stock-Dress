5e4bbe046d2f230f455dbf0da6a794b26ec6aad3
Md-Ataur-Rahman <mdataurrahmanm505@gmail.com>
2022-04-30 15:47:13 +0600

    Inventory Added

---
+++ src/Components/InventoryItems/InventoryItems.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Card, Button, Container } from "react-bootstrap";
+import StyleInventoryItems from "./InventoryItems.module.css";
+import "../Style/style.css";
+
+const InventoryItems = () => {
+  const [products, setProducts] = useState([]);
+  fetch("http://localhost:5000/products")
+    .then((res) => res.json())
+    .then((data) => setProducts(data));
+  return (
+    <Container>
+      <h2 className="section_header">Inventory Items</h2>
+      <div className={StyleInventoryItems.inventory_section}>
+        {products.slice(0, 6).map((pd) => (
+          <Card>
+            <div className={StyleInventoryItems.card_img_container}>
+              <img variant="top" src={pd.img} alt="" />
+            </div>
+            <Card.Body>
+              <Card.Title>{pd?.name}</Card.Title>
+              <Card.Text>{pd.shortText}</Card.Text>
+              <Card.Text>Price: {pd?.Price}</Card.Text>
+              <Card.Text>Quantity: {pd?.quantity}</Card.Text>
+              <Card.Text>Supplier Name: {pd?.supplierName}</Card.Text>
+              <Button variant="primary">Update It</Button>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default InventoryItems;
