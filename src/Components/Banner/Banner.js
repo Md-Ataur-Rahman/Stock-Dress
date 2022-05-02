bbdba76a34c2aea09afcab989dc59645d3b8dc9a
Md-Ataur-Rahman <mdataurrahmanm505@gmail.com>
2022-05-02 13:28:55 +0600

    some things new added

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import BannerStyle from "./Banner.module.css";
+import "../Style/style.css";
 const Banner = () => {
-  return <section className={BannerStyle.banner_section}>Banner</section>;
+  return (
+    <>
+      <section className={BannerStyle.banner_section}>Banner</section>
+      <section className={BannerStyle.banner_contactus_section}>
+        <Container>
+          <div className={BannerStyle.inner_banner_contactus_section}>
+            <p>REACH YOUR DESTINATION 100% SAFE & SECURE</p>
+            <button className="secondary_btn">Contact Us</button>
+          </div>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default Banner;
