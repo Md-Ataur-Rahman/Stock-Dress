672bd0c3c9b09914c7e3242733009337482d6a82
Md-Ataur-Rahman <mdataurrahmanm505@gmail.com>
2022-05-02 16:30:02 +0600

    banner responsive

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -5,7 +5,21 @@ import "../Style/style.css";
 const Banner = () => {
   return (
     <>
-      <section className={BannerStyle.banner_section}>Banner</section>
+      <section className={BannerStyle.banner_section}>
+        <Container>
+          <div className={BannerStyle.inner_banner_section}>
+            <h1>
+              Best Products Sell Now <br /> Dress Store
+            </h1>
+            <p>
+              Our productivities are best. We have million of clothes in our
+              store, Now We are selling this dress. Our Communities so widely
+              big. Now Are you buying it?
+            </p>
+            <button className="primary_btn">READ MORE</button>
+          </div>
+        </Container>
+      </section>
       <section className={BannerStyle.banner_contactus_section}>
         <Container>
           <div className={BannerStyle.inner_banner_contactus_section}>
