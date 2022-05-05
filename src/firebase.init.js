d406e0188eb70878d115d545fc5f37b8ba8885ec
Md-Ataur-Rahman <mdataurrahmanm505@gmail.com>
2022-05-06 00:24:56 +0600

    env file added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -2,31 +2,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_apiKey,
-//   authDomain: process.env.REACT_APP_authDomain,
-//   projectId: process.env.REACT_APP_projectId,
-//   storageBucket: process.env.REACT_APP_storageBucket,
-//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//   appId: process.env.REACT_APP_appId,
-//   measurementId: process.env.REACT_APP_measurementId,
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyBWobnhbdeMdNpz7F9q5dTuHUMdhGgSn2Y",
-  authDomain: "stock-dress.firebaseapp.com",
-  projectId: "stock-dress",
-  storageBucket: "stock-dress.appspot.com",
-  messagingSenderId: "499739065911",
-  appId: "1:499739065911:web:cc7bc80aa1bdb78f8409b2",
-  measurementId: "G-M1XD8DLK4V",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
-console.log(firebaseConfig.apiKey);
-console.log(firebaseConfig.authDomain);
-console.log(firebaseConfig.projectId);
-console.log(firebaseConfig.storageBucket);
-console.log(firebaseConfig.messagingSenderId);
-console.log(firebaseConfig.appId);
-console.log(firebaseConfig.measurementId);
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
