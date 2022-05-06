42ee61d0bd7d39875ba7df73e01b590d5628a469
Md-Ataur-Rahman <mdataurrahmanm505@gmail.com>
2022-05-06 23:32:01 +0600

    updateitem fetch eddited

--- src/Components/UpdateItem/UpdateItem.js
+++ src/Components/UpdateItem/UpdateItem.js
@@ -11,7 +11,7 @@ const UpdateItem = () => {
   const [addQuantity, setAddQuantity] = useState();
 
   useEffect(() => {
-    fetch(`https://pure-anchorage-54337.herokuapp.com/item/${id}`)
+    fetch(`https://pure-anchorage-54337.herokuapp.com/inventory/${id}`)
       .then((res) => res.json())
       .then((data) => setItem(data));
   }, [id]);
@@ -25,7 +25,7 @@ const UpdateItem = () => {
     const decreaseQuantity = item.quantity - 1;
     item.quantity = decreaseQuantity;
     setQuantity(decreaseQuantity);
-    fetch(`https://pure-anchorage-54337.herokuapp.com/item/${id}`, {
+    fetch(`https://pure-anchorage-54337.herokuapp.com/inventory/${id}`, {
       method: "PUT",
       body: JSON.stringify({
         quantity: decreaseQuantity,
@@ -46,7 +46,7 @@ const UpdateItem = () => {
     setQuantity(increaseQuantity);
     console.log(addQuantity);
 
-    fetch(`https://pure-anchorage-54337.herokuapp.com/item/${id}`, {
+    fetch(`https://pure-anchorage-54337.herokuapp.com/inventory/${id}`, {
       method: "PUT",
       body: JSON.stringify({
         quantity: increaseQuantity,
